086847f74844e615fe42083d4bddfbf1acc5477b
ssirrikh <13358109+Ssirrikh@users.noreply.github.com>
2023-10-29 16:23:09 -0700

    update nav+search to React + responsive design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//// 10/27
+// 0.5 hrs: research mobile device detection
+//// 10/28
+// 2.0 hrs: research React and read documentation
+// 0.5 hrs: set up React project and config for Github Pages
+// 0.5 hrs: begin update to CSS 3; begin dev mobile web app in React
+//// 10/29
+// 1.5 hrs: incorporate responsive design and fluid typography for mobile web app
+
+function NavBar () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='NavBar' className='NavBar'>
+      English-Chukchansi Dictionary
     </div>
   );
 }
+
+function SearchPanel () {
+  return (
+    <div className='SearchPanel'>
+      <input type='text' id='SearchBar' className='SearchBar' placeholder='Search...' />
+    </div>
+  );
+}
+
+function App () {
+  return (
+    <div className='App'>
+      <NavBar />
+      <SearchPanel />
+    </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
